3a806827fd81ccab4b0b172014837f0958a8e9f3
yarara <yarara@users.noreply.github.com>
2014-10-03 01:00:44 +0400

    Update tasks.py

--- galleryapp/tasks.py
+++ galleryapp/tasks.py
@@ -7,19 +7,18 @@ from galleryapp.models import *
 
 @task
 def send_function(user, args):
-    args['req'] = user
+
     useri = User.objects.get(username = user)
-    args['mail'] = useri.email
+
     message = EmailMessage('Архив изображений', '', MANAGER_FROM_EMAIL, [useri.email])
-    args['from'] = MANAGER_FROM_EMAIL
-    args['qwe'] = useri
+
     try:
         im = Image.objects.filter(user = useri)
-        args['im1'] = im
+
     except:
         args['notuser'] = 'Нет файлов для отправки'
     else:
         for i in im:
-            args['qwe1'] = message.attach_file(i.image.path)
+            message.attach_file(i.image.path)
     
     return args
